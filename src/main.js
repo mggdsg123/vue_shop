cceff5059927e17175122b673a9c8728431196e9
guoguo <mggdsg123@gmail.com>
2019-10-26 17:17:06 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,10 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 全局样式
+import './assets/css/global.css'
+// 字体图标
+import './assets/fonts/iconfont.css'
+// 导入axios
+import axios from 'axios'
+// 设置请求根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+    // 挂载到原型上
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
